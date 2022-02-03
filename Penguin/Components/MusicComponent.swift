b4bc19fd3b8d6e08010ac291766af19f9cced780
Matheus <matheusav@outlook.com.br>
2022-02-03 08:30:08 -0300

    Correcting the issues from GitLab

--- Penguin/Components/MusicComponent.swift
+++ Penguin/Components/MusicComponent.swift
@@ -2,24 +2,19 @@ import SceneKit
 import GameplayKit
 
 class MusicComponent: GKComponent {
-    private var node: SCNNode?
-    
-    override func didAddToEntity() {
-        guard let node = entity?.component(ofType: GeometryComponent.self)?.node else {
-            return
-        }
-        
-        self.node = node
+    private var node: SCNNode? {
+        entity?.component(ofType: GeometryComponent.self)?.node
+    }
 
+    override func didAddToEntity() {
         if let music = SCNAudioSource(named: "JazzRush.mp3") {
             music.volume = 0.04
             let action = SCNAction.playAudio(music, waitForCompletion: true)
             
             let foreverAction = SCNAction.repeatForever(action)
-            node.runAction(foreverAction)
+            node?.runAction(foreverAction)
         } else {
             print("cannot find musical file")
         }
     }
-   
 }
