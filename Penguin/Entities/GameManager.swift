42c10b5d558a2a04c4350ec023eea3e7e28adef5
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2022-02-03 08:46:57 -0300

    Make GameManager the renderer delegate of the scene instead of the view controller

--- Penguin/Entities/GameManager.swift
+++ Penguin/Entities/GameManager.swift
@@ -13,7 +13,30 @@ protocol GameManagerDelegate: AnyObject {
 
 class GameManager: GKEntity {
 
+    // MARK: - Singleton logic
+
     static let shared = GameManager()
+
+    private override init() {
+        super.init()
+        addComponent(speedManager)
+        addComponent(scoreManager)
+    }
+
+    // MARK: - Entity components
+
+    let speedManager = SpeedManagerComponent()
+    let scoreManager = ScoreManagerComponent()
+
+    // MARK: - Other entities components
+
+    var playerHealth: PlayerHealthComponent?
+    var playerMovement: PlayerMovementComponent?
+
+    // MARK: - Game management
+
+    weak var delegate: GameManagerDelegate?
+    weak var scene: GameScene?
 
     var state: GameState = .paused {
         didSet {
@@ -26,34 +49,6 @@ class GameManager: GKEntity {
                 delegate?.didEnterDeathState()
             }
         }
-    }
-
-    weak var delegate: GameManagerDelegate?
-
-    weak var scene: GameScene?
-
-    let speedManager = SpeedManagerComponent()
-    let scoreManager = ScoreManagerComponent()
-
-    var playerHealth: PlayerHealthComponent?
-    var playerMovement: PlayerMovementComponent?
-
-    var currentSpeed: Speed {
-        speedManager.currentSpeed
-    }
-
-    var currentScore: Int {
-        scoreManager.score
-    }
-
-    var coinValue: Int {
-        Int(currentSpeed.rawValue) * Config.baseCoinValue
-    }
-
-    private override init() {
-        super.init()
-        addComponent(speedManager)
-        addComponent(scoreManager)
     }
 
     func togglePause() {
@@ -73,7 +68,39 @@ class GameManager: GKEntity {
         scoreManager.resetScore()
     }
 
+    // MARK: - Access to game information
+
+    var currentSpeed: Speed {
+        speedManager.currentSpeed
+    }
+
+    var currentScore: Int {
+        scoreManager.score
+    }
+
+    var coinValue: Int {
+        Int(currentSpeed.rawValue) * Config.baseCoinValue
+    }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension GameManager: SCNSceneRendererDelegate {
+
+    /// Basic scene update method, called by SceneKit every beginning of a new cycle if the scene is not paused.
+    /// In this case, this should mean that it won't be called if `state == .paused`.
+    /// - Parameters:
+    ///   - renderer: Scene renderer.
+    ///   - time: Absolute real life time.
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard GameManager.shared.state == .playing else {
+            return
+        }
+
+        scene?.entities.forEach { entity in
+            entity.update(deltaTime: time)
+        }
     }
 }
--- Penguin/Scenes/ViewControllers/GameViewController.swift
+++ Penguin/Scenes/ViewControllers/GameViewController.swift
@@ -31,7 +31,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.scene = gameScene
-        sceneView.delegate = self
+        sceneView.delegate = GameManager.shared
         sceneView.isPlaying = true
 
         GameManager.shared.delegate = self
@@ -94,13 +94,6 @@ class GameViewController: UIViewController {
 
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchesEnded(touches, with: event)
-    }
-}
-
-extension GameViewController: SCNSceneRendererDelegate {
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        guard GameManager.shared.state == .playing else { return }
-        gameScene.entities.forEach { $0.components.forEach { $0.update(deltaTime: time) } }
     }
 }
 
