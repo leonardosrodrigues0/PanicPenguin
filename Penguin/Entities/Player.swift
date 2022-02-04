42ec8ce96a6272526d78690c1f66ab44816b005b
Eduardo <ediasdini@apple.com>
2022-02-04 11:12:48 -0300

    Update Player.swift

--- Penguin/Entities/Player.swift
+++ Penguin/Entities/Player.swift
@@ -18,7 +18,6 @@ class Player: GKEntity {
         body.angularVelocityFactor = SCNVector3(1, 0, 0)
         body.categoryBitMask = PhysicsCategory.player.rawValue
         body.collisionBitMask = PhysicsCategory.bitMask(forCategories: [
-            PhysicsCategory.ground
 //            PhysicsCategory.obstacle
         ])
 
@@ -52,24 +51,24 @@ class Player: GKEntity {
     override init() {
         super.init()
         addComponent(GeometryComponent(geometry: Self.geometry, position: .init(0, 0.25, -25)))
-//        addComponent(PhysicsComponent(withBody: Self.physicsBody))
+        addComponent(PhysicsComponent(withBody: Self.physicsBody))
         addComponent(PlayerMovementComponent())
         let healthComponent = PlayerHealthComponent()
         addComponent(healthComponent)
         addComponent(animationComponent)
-//        addComponent(ContactComponent(with: [.obstacle, .coin, .powerUp], { category in
-//            switch category {
-//            case .obstacle:
-//                self.animationComponent.run(.hit)
-//                healthComponent.hit()
-//            case .powerUp:
-//                GameManager.shared.speedManager.changeSpeed(to: .v5)
-//            case .coin:
-//                GameManager.shared.scoreManager.collectCoin()
-//            default:
-//                return
-//            }
-//        }))
+        addComponent(ContactComponent(with: [.obstacle, .coin, .powerUp], { category in
+            switch category {
+            case .obstacle:
+                self.animationComponent.run(.hit)
+                healthComponent.hit()
+            case .powerUp:
+                GameManager.shared.speedManager.changeSpeed(to: .v5)
+            case .coin:
+                GameManager.shared.scoreManager.collectCoin()
+            default:
+                return
+            }
+        }))
     }
 
     required init?(coder: NSCoder) {
