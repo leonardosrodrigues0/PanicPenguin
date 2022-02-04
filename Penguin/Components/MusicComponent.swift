791ebea1972a2116be3c506aa6dde960b7f1764c
Matheus <matheusav@outlook.com.br>
2022-02-04 09:00:15 -0300

    SoundEffect and MusicBackgroung implementation

--- Penguin/Components/MusicComponent.swift
+++ Penguin/Components/MusicComponent.swift
@@ -5,16 +5,40 @@ class MusicComponent: GKComponent {
     private var node: SCNNode? {
         entity?.component(ofType: GeometryComponent.self)?.node
     }
-
-    override func didAddToEntity() {
-        if let music = SCNAudioSource(named: "JazzRush.mp3") {
-            music.volume = 0.04
-            let action = SCNAction.playAudio(music, waitForCompletion: true)
-            
-            let foreverAction = SCNAction.repeatForever(action)
-            node?.runAction(foreverAction)
+    
+    func hitObstacleSound() {
+        if GameManager.shared.currentSpeed == .v1 {
+            if let soundEffect = SCNAudioSource(named: "Pinguim-dead.mp3") {
+                let action = SCNAction.playAudio(soundEffect, waitForCompletion: false)
+                node?.runAction(action)
+            } else {
+                print("Sound Effect of pinguim died wasn't find")
+            }
         } else {
-            print("cannot find musical file")
+            if let soundEffect = SCNAudioSource(named: "Pinguim-strike.mp3" ) {
+                let action = SCNAction.playAudio(soundEffect, waitForCompletion: false)
+                node?.runAction(action)
+            } else {
+                print("Sound Effect of pinguim strike wasn't find")
+            }
+        }
+    }
+    
+    func powerUpSound() {
+        if let soundEffect = SCNAudioSource(named: "Pinguim-powerUp.mp3") {
+            let action = SCNAction.playAudio(soundEffect, waitForCompletion: false)
+            node?.runAction(action)
+        } else {
+            print("Sound Effect of powerUp wasn't found")
+        }
+    }
+    
+    func coinSound() {
+        if let soundEffect = SCNAudioSource(named: "Pinguim-coin.mp3") {
+            let action = SCNAction.playAudio(soundEffect, waitForCompletion: false)
+            node?.runAction(action)
+        } else {
+            print("Sound Effect of pinguim died wasn't find")
         }
     }
 }
--- Penguin/Scenes/ViewControllers/GameViewController.swift
+++ Penguin/Scenes/ViewControllers/GameViewController.swift
@@ -1,19 +1,21 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import AVFoundation
 
 class GameViewController: UIViewController {
-
+    
     @IBOutlet private var sceneView: SCNView!
     @IBOutlet private var scoreLabel: UILabel!
     @IBOutlet private var speedLabel: UILabel!
-
+    
     @IBAction private func pause() {
         GameManager.shared.togglePause()
     }
-
+    
     lazy private var gameScene: GameScene = buildNewScene()
-
+    public var backgroundMusicPlayer: AVAudioPlayer?
+    
     private func buildNewScene() -> GameScene {
         let scene = GameScene()
         GameManager.shared.scene = scene
@@ -24,50 +26,51 @@ class GameViewController: UIViewController {
         scene.add(Spawner<Tree>())
         scene.add(Spawner<Coin>())
         scene.add(Spawner<SpeedPowerUp>())
-
+        playBackgroundMusic()
+        
         return scene
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.scene = gameScene
         sceneView.delegate = self
         sceneView.isPlaying = true
-
+        
         GameManager.shared.delegate = self
-
+        
         DispatchQueue.main.async {
             let alert = self.buildControllerChoiceAlert()
             self.present(alert, animated: true)
         }
-
+        
         Timer.scheduledTimer(withTimeInterval: Config.interval, repeats: true) { _ in
             self.scoreLabel.text = "\(GameManager.shared.currentScore)"
             self.speedLabel.text = "\(GameManager.shared.currentSpeed)"
         }
     }
-
+    
     func buildControllerChoiceAlert() -> UIAlertController {
         let alert = UIAlertController(title: "Choose your Controller Scheme", message: nil, preferredStyle: .actionSheet)
-
+        
         let setMotionControllerAction = UIAlertAction(title: "Motion", style: .default) { _ in
             GameManager.shared.playerMovement?.controllerType = .motion
             GameManager.shared.state = .playing
         }
-
+        
         let setTouchControllerAction = UIAlertAction(title: "Touch", style: .default) { _ in
             GameManager.shared.playerMovement?.controllerType = .touch
             GameManager.shared.state = .playing
         }
-
+        
         alert.addAction(setTouchControllerAction)
         alert.addAction(setMotionControllerAction)
-
+        
         return alert
     }
-
+    
     // MARK: - Touches Handling Methods
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         GameManager.shared.playerMovement?.movementManager?.touchesBegan(
             touches,
@@ -75,7 +78,7 @@ class GameViewController: UIViewController {
             view: sceneView
         )
     }
-
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         GameManager.shared.playerMovement?.movementManager?.touchesEnded(
             touches,
@@ -83,7 +86,7 @@ class GameViewController: UIViewController {
             view: sceneView
         )
     }
-
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         GameManager.shared.playerMovement?.movementManager?.touchesMoved(
             touches,
@@ -91,7 +94,7 @@ class GameViewController: UIViewController {
             view: sceneView
         )
     }
-
+    
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchesEnded(touches, with: event)
     }
@@ -107,27 +110,47 @@ extension GameViewController: SCNSceneRendererDelegate {
 extension GameViewController: GameManagerDelegate {
     func didEnterDeathState() {
         let alert = buildResetGameAlert()
-
+        
         DispatchQueue.main.async {
             self.present(alert, animated: true)
         }
     }
-
+    
     private func buildResetGameAlert() -> UIAlertController {
         let alert = UIAlertController(title: "You are now deceased.", message: nil, preferredStyle: .actionSheet)
-
+        
         let resetGameAction = UIAlertAction(title: "Reset Game", style: .default) { _ in
-
+            
             GameManager.shared.reset()
-
+            
             let scene = self.buildNewScene()
             self.gameScene = scene
             self.sceneView.scene = scene
             self.viewDidLoad()
         }
-
+        
         alert.addAction(resetGameAction)
-
+        
         return alert
+    }
+    
+    public func playBackgroundMusic() {
+        if let url = Bundle.main.url(forResource: "JazzRush", withExtension: ".mp3") {
+            do {
+                backgroundMusicPlayer = try AVAudioPlayer(contentsOf: url)
+            } catch {
+                print("the audio wasn't found")
+            }
+        } else {
+            return print("Could not find file: JazzRush")
+        }
+        if let player = backgroundMusicPlayer {
+            player.volume = 0.06
+            player.numberOfLoops = -1
+            player.prepareToPlay()
+            player.play()
+        } else {
+            print("Could not create audio player")
+        }
     }
 }
