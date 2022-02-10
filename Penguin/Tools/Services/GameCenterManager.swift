c9ae9500ff1c9f54f26e8da3f78cc00c18261c1d
Gabriel Muelas <bimuelas@gmail.com>
2022-02-10 18:17:58 -0300

    Implement a local record system

--- Penguin/Tools/Services/GameCenterManager.swift
+++ Penguin/Tools/Services/GameCenterManager.swift
@@ -9,8 +9,15 @@ class GameCenterManager: NSObject {
     
     static let shared = GameCenterManager()
     
+    /// A ViewController responsible for presenting Game Center views
     var viewController: UIViewController?
     
+    /// Checks if the local player is authenticated in Game Center. If not, then he couldn't post score to leaderboard
+    var isAuthenticated: Bool {
+        GKLocalPlayer.local.isAuthenticated
+    }
+    
+    /// Stores the player highest score in the user defaults
     private(set) var highestScore: Int {
         get {
             UserDefaults.standard.integer(forKey: "highestScore")
@@ -23,8 +30,9 @@ class GameCenterManager: NSObject {
     private override init() {
         super.init()
         GKLocalPlayer.local.authenticateHandler = { vc, error in
-            if GKLocalPlayer.local.isAuthenticated {
+            if self.isAuthenticated {
                 print("Usuário autenticado")
+                self.updateLocalBest(from: .test)
             } else if let vc = vc {
                 self.viewController?.present(vc, animated: true, completion: nil)
             } else if let error = error {
@@ -32,7 +40,6 @@ class GameCenterManager: NSObject {
             } else {
                 print("Erro ao autenticar usuário ao GameCenter")
             }
-            
         }
     }
     
@@ -42,17 +49,44 @@ class GameCenterManager: NSObject {
         viewController?.present(vc, animated: true, completion: nil)
     }
     
+    /// Checks if the score received is better than the player's record. If so, it publishes the new record to Game Center
     func score(_ value: Int) {
+        guard isAuthenticated else {
+            return
+        }
+        
         if value > highestScore {
-            print("Novo record")
             highestScore = value
             postScore(value, to: .test)
         }
     }
     
+    /// Publish the score to Game Center
     private func postScore(_ score: Int, to leaderboardID: LeaderboardType) {
         GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [leaderboardID.rawValue]) { error in
-            print(error?.localizedDescription ?? "Score has been posted to \(leaderboardID.rawValue)")
+            print(error?.localizedDescription ?? "Score has been posted to \(leaderboardID.rawValue): \(score)")
+        }
+    }
+    
+    /// Update the player highest score into storage
+    private func updateLocalBest(from leaderboardID: LeaderboardType) {
+        GKLeaderboard.loadLeaderboards(IDs: [leaderboardID.rawValue]) { leaderboards, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            leaderboards?.first?.loadEntries(for: [], timeScope: .allTime) { localEntry, _, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                if let entry = localEntry {
+                    print(entry.score)
+                    self.highestScore = entry.score
+                }
+            }
         }
     }
 }
