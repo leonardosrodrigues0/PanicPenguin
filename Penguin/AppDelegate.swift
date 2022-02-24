3fbed39e05cf3dc4ad5d637dd4537fae47676e3c
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2022-02-24 09:26:27 -0300

    Force portrait mode

--- Penguin/AppDelegate.swift
+++ Penguin/AppDelegate.swift
@@ -5,6 +5,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var orientationLock = UIInterfaceOrientationMask.portrait
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return self.orientationLock
+    }
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
